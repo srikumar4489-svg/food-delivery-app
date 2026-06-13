f9d63e31a53717fbc7635622b56e8da6ab86231c
Selvakumar <srikumar4489@gmail.com>
2026-06-13 07:33:19 +0530

    Updated restaurant urls

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('add-restaurant/',add_restaurant,name='add_restaurant'),
+    path('',add_restaurant,name='add_restaurant'),
     path('restaurant-list/',restaurant_list,name='restaurant_list'),
     path('add-food/',add_food,name='add_food'),
     path('food-list/',food_list,name='food_list'),
